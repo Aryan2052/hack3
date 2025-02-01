415f34db469e8e21d621d1a8a2820c02c640df64
Spandan Deb <spandan.deb04@gmail.com>
2025-02-01 20:37:25 +0530

    Added About page

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ function About() {
     <div className="about-page">
       <h2>About Us</h2>
       <p>FoodLink is a platform that connects surplus food donors with local shelters to help those in need...</p>
+      <p>Copyright 2024</p>
     </div>
   );
 }
