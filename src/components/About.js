d9a747b0eb409f8889bf5bef173ad69191a896b3
Aryan2052 <aryandangat2005@gmail.com>
2025-02-01 20:22:06 +0530

    Initial commit 1

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about-page">
+      <h2>About Us</h2>
+      <p>FoodLink is a platform that connects surplus food donors with local shelters to help those in need...</p>
+    </div>
+  );
+}
+
+export default About;
